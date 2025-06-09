f39e6e94bc0100d84c9d3d96a3889c54d7cf86fd
ShotReak <snehilgautam9@gmail.com>
2025-06-10 02:11:54 +0530

    made some progress with the sidebar

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="">
+      <div className="h-[90%] flex">
+        <Sidebar />
+      </div>
+    </div>
+  )
+}
+
+export default App
